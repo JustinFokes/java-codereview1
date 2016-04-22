adbe54bf05eae46d87f5ac664455a456b5f37fce
JustinFokes <justinfokes@Justins-MacBook-Pro.local>
2016-04-22 10:53:44 -0700

    add integration testing

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.*;
 import java.util.Map;
@@ -25,10 +26,38 @@ public class WordPuzzle {
       		model.put("template", "templates/score.vtl");
       		return new ModelAndView(model, "templates/layout.vtl");
     	}, 	new VelocityTemplateEngine());
-	}	
-		public static String wordPuzzleMethod(String input){
+
+		    get("/score2", (request, response) -> {
+		    HashMap<String, Object> model = new HashMap<String, Object>();
+		    String userInput1 = request.queryParams("user2");
+		    String userInput2 = request.queryParams("user1");
+		    WordPuzzle wordPuzzleTwo = new WordPuzzle();
+		    String outPutTwo = wordPuzzleTwo.guessMethod(userInput2, userInput1);
+		    model.put("outPutTwo", outPutTwo);
+
+      		model.put("template", "templates/score2.vtl");
+      		return new ModelAndView(model, "templates/layout.vtl");
+    	}, 	new VelocityTemplateEngine());
+	}
+
+		public static String wordPuzzleMethod(String input) {
 		String newInput = new String(input).toLowerCase();
 		String finalOutPut = newInput.replaceAll("a", "-").replaceAll("e", "-").replaceAll("i", "-").replaceAll("o", "-").replaceAll("u", "-").replaceAll("y", "-");
 		return finalOutPut;
 	}	
+		public static String guessMethod(String finalOutPut, String input) {
+		String firstInput = new String(finalOutPut).toLowerCase();
+		String userGuess = new String(input).toLowerCase(); 
+		if(firstInput.equals(userGuess)) {
+		return "Look's like we have a match!";
+		}else{
+		return "Wrong guess!";
+		}
+	}
 }
+
+
+
+
+
+
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -6,38 +6,46 @@ import static org.junit.Assert.*;
 public class test {
 
   	@Test
-  	public void testInput_takesSingleVowelInputAndReturnsDash_dash() {
+  	public void userInput_takesSingleVowelInputAndReturnsDash_dash() {
 	    WordPuzzle wordPuzzle = new WordPuzzle();
 	    String input = new String("a");
 	    String expected = "-";
 	    assertEquals(expected, wordPuzzle.wordPuzzleMethod(input));
   }
     @Test
-  	public void testInput_takesDoubleVowelInputAndReturnsDash_dash() {
+  	public void userInput_takesDoubleVowelInputAndReturnsDash_dash() {
 	    WordPuzzle wordPuzzle = new WordPuzzle();
 	    String input = new String("app tap");
 	    String expected = "-pp t-p";
 	    assertEquals(expected, wordPuzzle.wordPuzzleMethod(input));
   }
     @Test
-	public void testInput_takesTwoDifferentVowelInputsAndReturnsDash_dash() {
+	public void userInput_takesTwoDifferentVowelInputsAndReturnsDash_dash() {
 	    WordPuzzle wordPuzzle = new WordPuzzle();
 	    String input = new String("apple");
 	    String expected = "-ppl-";
 	    assertEquals(expected, wordPuzzle.wordPuzzleMethod(input));
   }
     @Test
-  	public void testInput_takesAllVowelInputsAndReturnsDash_dash() {
+  	public void userInput_takesAllVowelInputsAndReturnsDash_dash() {
     	WordPuzzle wordPuzzle = new WordPuzzle();
     	String input = new String("an apple is tasty in my mouth");
     	String expected = "-n -ppl- -s t-st- -n m- m--th";
     	assertEquals(expected, wordPuzzle.wordPuzzleMethod(input));
   }
     @Test
-  	public void testFinal_takesAllVowelInputsOfDifferentCasesAndReturnsDash_dash() {
+  	public void userInput_takesAllVowelInputsOfDifferentCasesAndReturnsDash_dash() {
     	WordPuzzle wordPuzzle = new WordPuzzle();
     	String input = new String("an Apple Is tasty In my moUth");
     	String expected = "-n -ppl- -s t-st- -n m- m--th";
     	assertEquals(expected, wordPuzzle.wordPuzzleMethod(input));
+  }
+  	@Test
+  	public void userGuess_compareStringGuessWithStringInput_true() {
+  		WordPuzzle wordPuzzle = new WordPuzzle();
+  		String input1 = new String("a");
+  		String input2 = new String("a");
+  		String expected = "Look's like we have a match!";
+  		assertEquals(expected, wordPuzzle.guessMethod(input1, input2));
   }
 }
