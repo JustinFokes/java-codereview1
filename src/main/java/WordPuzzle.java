51abb1b3ba04db99817100d72f0807d68ae4051b
JustinFokes <justinfokes@Justins-MacBook-Pro.local>
2016-04-21 22:46:42 -0700

    will add front end user interface

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -8,12 +8,30 @@ import static spark.Spark.*;
 
 public class WordPuzzle {
 		public static void main(String[] args) {
+			get("/", (request, response) -> {
+      		HashMap<String, Object> model = new HashMap<String, Object>();
+      		model.put("template", "templates/home.vtl");
+      		return new ModelAndView(model, "templates/layout.vtl");
+    	}, 	new VelocityTemplateEngine());
+
+		    get("/score", (request, response) -> {
+		    HashMap<String, Object> model = new HashMap<String, Object>();
+
+		    String userInput1 = request.queryParams("user1");
+		    WordPuzzle wordPuzzle = new WordPuzzle();
+		    String outPut = wordPuzzle.wordPuzzleMethod(userInput1);
+		    model.put("outPut", outPut);
+
+      		model.put("template", "templates/score.vtl");
+      		return new ModelAndView(model, "templates/layout.vtl");
+    	}, 	new VelocityTemplateEngine());
 	}	
 		public static String wordPuzzleMethod(String input){
-		
 		String newInput = new String(input).toLowerCase();
 		String finalOutPut = newInput.replaceAll("a", "-").replaceAll("e", "-").replaceAll("i", "-").replaceAll("o", "-").replaceAll("u", "-").replaceAll("y", "-");
 		return finalOutPut;
 	}	
 
 }
+
+//fairly sure that I
